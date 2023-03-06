f0dbd3f056fd28283009cdfa64a8467e60425f29
GJanos <janos.gorondi.official@gmail.com>
2023-03-06 23:39:39 +0100

    first commit

---
+++ microwave/build/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/gr20j/JanosGorondi/FONTYS/t-sem3-cb-master/sd/7_OOA_and_OOD/Microwave/lib/googletest
+# Build directory: C:/Users/gr20j/JanosGorondi/FONTYS/t-sem3-cb-master/sd/7_OOA_and_OOD/Microwave/build/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
