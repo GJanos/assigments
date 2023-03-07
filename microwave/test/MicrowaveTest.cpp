494922f647e2a4f1aff8543607419a5d6876c3a0
GJanos <janos.gorondi.official@gmail.com>
2023-03-07 19:58:52 +0100

    23

--- microwave/test/MicrowaveTest.cpp
+++ microwave/test/MicrowaveTest.cpp
@@ -80,3 +80,63 @@ TEST_F(MicrowaveTest, heating_power_change)
     microwave.HandleEvent(EV_START);
     EXPECT_EQ(STATE_HEATING, microwave.HandleHeatingState(EV_POWER_CHANGE));
 }
+
+TEST_F(MicrowaveTest, idle_start)
+{
+    EXPECT_CALL(ui, GetRequestedPower()).WillOnce(Return(800));
+    EXPECT_CALL(motor, SetPower(800));
+    microwave.HandleEvent(EV_START);
+    EXPECT_EQ(STATE_HEATING, microwave.HandleHeatingState(EV_POWER_CHANGE));
+
+    // EXPECT_EQ(STATE_HEATING, microwave.HandleIdleState(EV_START));
+}
+/*
+TEST_F(MicrowaveTest, idle_door_open)
+{
+    EXPECT_EQ(STATE_DOOR_OPENED, microwave.HandleIdleState(EV_DOOR_OPEN));
+}
+
+TEST_F(MicrowaveTest, door_open_door_close)
+{
+    microwave.HandleEvent(EV_DOOR_OPEN);
+    EXPECT_EQ(STATE_IDLE, microwave.HandleDoorOpenedState(EV_DOOR_CLOSE));
+}
+
+TEST_F(MicrowaveTest, door_open_start)
+{
+    microwave.HandleEvent(EV_DOOR_OPEN);
+    EXPECT_EQ(STATE_PAUSE, microwave.HandleDoorOpenedState(EV_START));
+}
+
+TEST_F(MicrowaveTest, pause_clk_reset)
+{
+    microwave.HandleEvent(EV_DOOR_OPEN);
+    microwave.HandleEvent(EV_START);
+    EXPECT_EQ(STATE_DOOR_OPENED, microwave.HandlePauseState(EV_CLOCK_RESET));
+}
+
+TEST_F(MicrowaveTest, pause_door_close)
+{
+    microwave.HandleEvent(EV_DOOR_OPEN);
+    microwave.HandleEvent(EV_START);
+    EXPECT_EQ(STATE_HEATING, microwave.HandlePauseState(EV_DOOR_CLOSE));
+}
+
+TEST_F(MicrowaveTest, heating_timer_up)
+{
+    microwave.HandleEvent(EV_START);
+    EXPECT_EQ(STATE_IDLE, microwave.HandleHeatingState(EV_TIME_UP));
+}
+
+TEST_F(MicrowaveTest, heating_door_open)
+{
+    microwave.HandleEvent(EV_START);
+    EXPECT_EQ(STATE_PAUSE, microwave.HandleHeatingState(EV_DOOR_OPEN));
+}
+
+TEST_F(MicrowaveTest, heating_power_change)
+{
+    microwave.HandleEvent(EV_START);
+    EXPECT_EQ(STATE_HEATING, microwave.HandleHeatingState(EV_POWER_CHANGE));
+}
+*/
