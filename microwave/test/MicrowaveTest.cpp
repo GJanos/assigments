68614eab52efdf9f61a9294e6c216469abfe7651
GJanos <janos.gorondi.official@gmail.com>
2023-03-07 19:29:02 +0100

    2

--- microwave/test/MicrowaveTest.cpp
+++ microwave/test/MicrowaveTest.cpp
@@ -25,35 +25,58 @@ protected:
     Microwave microwave;
 };
 
-TEST_F(MicrowaveTest, test_start_event)
+TEST_F(MicrowaveTest, idle_start)
 {
     // EXPECT_CALL(ui, GetRequestedPower()).WillOnce(Return(800));
     // EXPECT_CALL(motor, SetPower(800));
     EXPECT_EQ(STATE_HEATING, microwave.HandleIdleState(EV_START));
 }
 
-TEST_F(MicrowaveTest, test_door_open_event)
+TEST_F(MicrowaveTest, idle_door_open)
 {
     EXPECT_EQ(STATE_DOOR_OPENED, microwave.HandleIdleState(EV_DOOR_OPEN));
 }
 
-TEST_F(MicrowaveTest, test_door_close_event)
+TEST_F(MicrowaveTest, door_open_door_close)
 {
     microwave.HandleEvent(EV_DOOR_OPEN);
     EXPECT_EQ(STATE_IDLE, microwave.HandleDoorOpenedState(EV_DOOR_CLOSE));
 }
-/*
 
-TEST_F(MicrowaveTest, test_door_open_event)
+TEST_F(MicrowaveTest, door_open_start)
 {
-    EXPECT_EQ(STATE_DOOR_OPENED, microwave.HandleIdleState(EV_DOOR_OPEN));
+    microwave.HandleEvent(EV_DOOR_OPEN);
+    EXPECT_EQ(STATE_PAUSE, microwave.HandleDoorOpenedState(EV_START));
 }
-TEST_F(MicrowaveTest, test_door_open_event)
+
+TEST_F(MicrowaveTest, pause_clk_reset)
 {
-    EXPECT_EQ(STATE_DOOR_OPENED, microwave.HandleIdleState(EV_DOOR_OPEN));
+    microwave.HandleEvent(EV_DOOR_OPEN);
+    microwave.HandleEvent(EV_START);
+    EXPECT_EQ(STATE_DOOR_OPENED, microwave.HandlePauseState(EV_CLOCK_RESET));
 }
-TEST_F(MicrowaveTest, test_door_open_event)
+
+TEST_F(MicrowaveTest, pause_door_close)
 {
-    EXPECT_EQ(STATE_DOOR_OPENED, microwave.HandleIdleState(EV_DOOR_OPEN));
+    microwave.HandleEvent(EV_DOOR_OPEN);
+    microwave.HandleEvent(EV_START);
+    EXPECT_EQ(STATE_HEATING, microwave.HandlePauseState(EV_DOOR_CLOSE));
 }
-*/
+
+TEST_F(MicrowaveTest, heating_timer_up)
+{
+    microwave.HandleEvent(EV_START);
+    EXPECT_EQ(STATE_IDLE, microwave.HandleHeatingState(EV_TIME_UP));
+}
+
+TEST_F(MicrowaveTest, heating_door_open)
+{
+    microwave.HandleEvent(EV_START);
+    EXPECT_EQ(STATE_PAUSE, microwave.HandleHeatingState(EV_DOOR_OPEN));
+}
+
+TEST_F(MicrowaveTest, heating_power_change)
+{
+    microwave.HandleEvent(EV_START);
+    EXPECT_EQ(STATE_HEATING, microwave.HandleHeatingState(EV_POWER_CHANGE));
+}
