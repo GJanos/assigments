386ad15397d2fcb5a028ebf5d054c30951e63e0d
GJanos <janos.gorondi.official@gmail.com>
2023-03-13 13:29:13 +0100

    que

--- ass3/myqueue/product/myqueue.c
+++ ass3/myqueue/product/myqueue.c
@@ -2,14 +2,34 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdint.h>
-#include <unistd.h>
+// #include <unistd.h>
 
 #include "mystack.h"
 #include "myqueue.h"
 
 QueueMeta_t *myqueue_create(int item_size)
 {
-	return NULL;
+	if (item_size < 0)
+		return NULL;
+	QueueMeta_t *new_queue = (QueueMeta_t *)malloc(sizeof(QueueMeta_t));
+	if (new_queue == NULL)
+		return NULL;
+	new_queue->stack_in = (StackMeta_t *)malloc(sizeof(StackMeta_t));
+	if (new_queue->stack_in == NULL)
+		return NULL;
+	new_queue->stack_out = (StackMeta_t *)malloc(sizeof(StackMeta_t));
+	if (new_queue->stack_out == NULL)
+		return NULL;
+	new_queue->item_size = item_size;
+
+	new_queue->stack_in->numelem = 0;
+	new_queue->stack_in->objsize = item_size;
+	new_queue->stack_in->stack = NULL;
+	new_queue->stack_out->numelem = 0;
+	new_queue->stack_out->objsize = item_size;
+	new_queue->stack_out->stack = NULL;
+
+	return new_queue;
 }
 
 void myqueue_delete(QueueMeta_t *queue)
@@ -26,7 +46,3 @@ int myqueue_dequeue(QueueMeta_t *queue, void *obj)
 {
 	return 0;
 }
-
-
-
-
--- ass3/myqueue/test/myqueue_test.c
+++ ass3/myqueue/test/myqueue_test.c
@@ -8,25 +8,33 @@
 // I rather dislike keeping line numbers updated, so I made my own macro to ditch the line number
 #define MY_RUN_TEST(func) RUN_TEST(func, 0)
 
+QueueMeta_t *queue;
 void setUp(void)
 {
+	queue = NULL;
 }
 
 void tearDown(void)
 {
-    // This is run after EACH test
+	// This is run after EACH test
 }
 
 void test_queue_create_delete(void)
 {
+	queue = myqueue_create(sizeof(int));
+	TEST_ASSERT_NOT_EQUAL(NULL, queue);
+	TEST_ASSERT_NOT_EQUAL(NULL, queue->stack_in);
+	TEST_ASSERT_NOT_EQUAL(NULL, queue->stack_out);
+	TEST_ASSERT_EQUAL(0, queue->stack_in->numelem + queue->stack_out->numelem);
+	TEST_ASSERT_EQUAL(sizeof(int), queue->item_size);
 }
 
 void test_enqueue_dequeue(void)
 {
 }
 
-
-int main () {
+int main()
+{
 	UnityBegin();
 	MY_RUN_TEST(test_queue_create_delete);
 	MY_RUN_TEST(test_enqueue_dequeue);
